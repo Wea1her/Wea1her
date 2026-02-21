d23d67ed639ad8ecc3ee97f620ed47d8e440f030
Wea1her <h503859571@gmail.com>
2026-02-21 22:09:14 +0800

    chore: update site configuration

--- src/site.config.ts
+++ src/site.config.ts
@@ -55,7 +55,7 @@ export const theme: ThemeUserConfig = {
   header: {
     menu: [
       { title: 'Blog', link: '/blog' },
-      { title: 'Docs', link: 'https://docs.0xweather.com/vibe-coding/01' },
+      { title: 'Docs', link: 'https://docs.0xweather.com/' },
       { title: 'Projects', link: '/projects' },
       { title: 'Links', link: '/links' },
       { title: 'About', link: '/about' }
